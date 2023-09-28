29690571f8ed8b365428e9bd313f83169427cee1
souf139 <soufianboushaba12@gmail.com>
2023-09-28 12:12:38 +0100

    this commit for challeneg4 in basics

---
+++ basics/challenge4/index.js
@@ -0,0 +1,18 @@
+
+    var a;
+    var b;
+    var c;
+    var d;
+    var sum ;
+	var moy;
+    
+    a = parseFloat(prompt("first number :"));
+    b = parseFloat(prompt("second number :"));
+    c = parseFloat(prompt("therd number :"));
+    d = parseFloat(prompt("fourth number :"));
+    
+    sum = a+b+c+d;
+    moy=sum/4;
+    
+    console.log(" the sum is : ", sum);
+    console.log(" the average is :", moy);
